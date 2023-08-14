fd141ad5da89bedc1772c4e27a50b06a68db6a72
nicomilsz <nico@Air-de-Fernanda.fibertel.com.ar>
2023-08-14 18:50:54 -0300

    Agrego rutas de local host a app.js. agrego formularios a registro.html, agrego puerto local a package.json y app.js

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const path = require('path')
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+const port=3001
+app.listen(port,()=> {
+    console.log(`Se prendio en el puerto ${port}`)
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+
+app.get("/detalle_de_producto",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+
+app.get("/carrito_de_compras",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
