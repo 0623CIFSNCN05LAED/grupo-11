e6599261ca25785a86827f330040bcfabaca1420
nicomilsz <nico@Air-de-Fernanda.fibertel.com.ar>
2023-08-14 19:13:18 -0300

    app.js

--- src/app.js
+++ src/app.js
@@ -15,19 +15,19 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/registro",(req,res)=>{
-    res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "views/registro.html"));
 });
 
 app.get("/login",(req,res)=>{
-    res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "views/login.html"));
 });
 
 
 app.get("/detalle_de_producto",(req,res)=>{
-    res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "views/detalle_de_producto.html"));
 });
 
 
 app.get("/carrito_de_compras",(req,res)=>{
-    res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "views/carrito_de_compras.html"));
 });
