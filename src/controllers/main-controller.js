e7e3260429bcd887dbd30375f3c77a39c9482ea8
Luciano Muga <lucho.muga2@gmail.com>
2023-09-08 15:02:22 -0300

    detalles

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,7 +1,11 @@
+const producto = require("../data/productos")
+
 module.exports = {
     home: (req, res) => res.render("index"),
     login: (req, res) => res.render("login"),
     registro: (req, res) => res.render("registro"),
-    detalle: (req, res) => res.render("detalle_de_producto"),
+    detalle: (req, res) => res.render("detalle_de_producto", {
+        detalles: producto,
+    }),
     carrito: (req, res) => res.render("carrito_de_compras")
 }
---
+++ src/data/productos.js
@@ -0,0 +1,27 @@
+productos = [{
+    name: "Hoodie super soft",
+    price: 4500,
+    discount: 25,
+    image: ".../public/images/buzo.jpg",
+},
+{
+    name:"Remera oversize",
+    price: 2000,
+    discount: 15,
+    image: ".../public/images/remera-oversize.jps"
+},
+{
+    name:"Campera de jean",
+    price: 7000,
+    discount: 20,
+    image: ".../public/images/campera-jean.jpg"
+},
+{
+    name:"Jogger",
+    price: 3000,
+    discount: 10,
+    image: ".../public/images/jogger.jpg"
+}
+]
+
+module.exports = productos
