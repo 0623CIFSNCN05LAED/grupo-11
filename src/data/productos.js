11389278a16227ad445aa506821c24ff97dd9a94
Luciano Muga <lucho.muga2@gmail.com>
2023-09-17 23:56:14 -0300

    detalles de productos imagenes agregadas

--- src/data/productos.js
+++ src/data/productos.js
@@ -2,25 +2,25 @@ productos = [{
     name: "Hoodie super soft",
     price: 4500,
     discount: 25,
-    image: ".../public/images/buzo.jpg",
+    image: "buzo.jpg",
 },
 {
     name:"Remera oversize",
     price: 2000,
     discount: 15,
-    image: ".../public/images/remera-oversize.jps"
+    image: "remera-oversize.jpg"
 },
 {
     name:"Campera de jean",
     price: 7000,
     discount: 20,
-    image: ".../public/images/campera-jean.jpg"
+    image: "campera-jean.jpg"
 },
 {
     name:"Jogger",
     price: 3000,
     discount: 10,
-    image: ".../public/images/jogger.jpg"
+    image: "jogger.jpg"
 }
 ]
 
