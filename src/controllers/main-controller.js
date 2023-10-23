0913e4111e3be186f80503fcf1e24dd0d07c6250
Luciano Muga <lucho.muga2@gmail.com>
2023-10-22 23:22:18 -0300

    rama Luciano

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,8 +1,4 @@
 const productoServices = require("../productServices/productServices")
-const {validationResult} = require("express-validator")
-const User = require("../data/User")
-const bcrypt = require("bcryptjs")
-const userServices = require("../productServices/userServices")
 
 module.exports = {
 
