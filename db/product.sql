f2c6b1f6748a88bf81cc8ecda72160417221f843
leandroheffes24 <leandroheffes@outlook.es>
2023-11-23 12:33:15 -0300

    Database corregida

--- db/product.sql
+++ db/product.sql
@@ -3,27 +3,24 @@ CREATE DATABASE `product`;
 USE `product`;
 
 CREATE TABLE `products` (
-  `id` smallint(6) NOT NULL,
-  `price` int(50) NOT NULL,
-  `discount` smallint(6) default null,
-  `product_name` varchar(95) not null,
-  `image` varchar(95) not null,
-  `created_at` timestamp NOT NULL DEFAULT now(),
-  `updated_at` timestamp NOT NULL DEFAULT now(),
-  PRIMARY KEY (`id`)
+    `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+    `price` int(50) NOT NULL,
+    `discount` smallint(6) default null,
+    `product_name` varchar(95) not null,
+    `image` varchar(95) not null,
+    `created_at` timestamp NOT NULL DEFAULT now(),
+    `updated_at` timestamp NOT NULL DEFAULT now(),
+    PRIMARY KEY (`id`)
 );
 
-
-
-
 create table `size` (
-	`id` smallint(9) not null,
- 	`size_name` varchar(95) not null,
- 	primary key (`id`)
+	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+	`size_name` varchar(95) not null,
+	primary key (`id`)
 );
 
 create table `users` (
-	`id` smallint(9) not null,
+	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
 	`name` varchar(96) not null,
 	`last_name` varchar(96) not null,
 	`email` varchar(96) not null,
@@ -34,9 +31,8 @@ create table `users` (
 	primary key (`id`)
 );
 
-
 create table `shopping_cart` ( 
-	`id` smallint(9) not null,
+	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
 	`image` varchar(95) not null,
 	`product_name` varchar(95) not null,
 	`id_product` smallint(9) not null,
@@ -52,9 +48,8 @@ create table `shopping_cart` (
 	CONSTRAINT `shopping_cart_FK` FOREIGN KEY (`id_users`) REFERENCES `users`(`id`)
 );
 
-
 create table `products_shopping_cart` (
-	`id` smallint(9) not null,
+	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
 	`id_product` smallint(9) not null,
 	`id_shopping_cart` smallint(9) not null,
 	primary key (`id`),
@@ -62,11 +57,10 @@ create table `products_shopping_cart` (
 	key `products_shopping_cart_FK_1` (`id_shopping_cart`),
 	CONSTRAINT `products_shopping_cart_FK` FOREIGN KEY (`id_product`) REFERENCES `products`(`id`),
 	CONSTRAINT `products_shopping_cart_FK_1` FOREIGN KEY (`id_shopping_cart`) REFERENCES `shopping_cart`(`id`)
-	
 );
 
 create table `products_size` (
-	`id` smallint(9) not null,
+	`id` smallint(6) unsigned NOT NULL auto_increment,
 	`id_product` smallint(6) not null,
 	`id_size` smallint(6) not null,
 	primary key (`id`),
@@ -74,7 +68,4 @@ create table `products_size` (
 	key `products_size_FK_1` (`id_size`),
 	CONSTRAINT `products_size_FK` FOREIGN KEY (`id_product`) REFERENCES `products`(`id`),
 	CONSTRAINT `products_size_FK_1` FOREIGN KEY (`id_size`) REFERENCES `size`(`id`)
-	
 );
-
-
