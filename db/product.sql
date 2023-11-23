1f18932dae7578ec1d739a33b476498f6afe6b75
leandroheffes24 <leandroheffes@outlook.es>
2023-11-23 13:26:47 -0300

    Corrección base de datos

--- db/product.sql
+++ db/product.sql
@@ -3,20 +3,20 @@ CREATE DATABASE `product`;
 USE `product`;
 
 CREATE TABLE `products` (
-    `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
-    `price` int(50) NOT NULL,
-    `discount` smallint(6) default null,
-    `product_name` varchar(95) not null,
-    `image` varchar(95) not null,
-    `created_at` timestamp NOT NULL DEFAULT now(),
-    `updated_at` timestamp NOT NULL DEFAULT now(),
-    PRIMARY KEY (`id`)
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `price` int(50) NOT NULL,
+  `discount` smallint(6) default null,
+  `product_name` varchar(95) not null,
+  `image` varchar(95) not null,
+  `created_at` timestamp NOT NULL DEFAULT now(),
+  `updated_at` timestamp NOT NULL DEFAULT now(),
+  PRIMARY KEY (`id`)
 );
 
 create table `size` (
 	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
-	`size_name` varchar(95) not null,
-	primary key (`id`)
+ 	`size_name` varchar(95) not null,
+ 	primary key (`id`)
 );
 
 create table `users` (
@@ -40,7 +40,7 @@ create table `shopping_cart` (
 	`subtotal` smallint(9) not null,
 	`shipment_cost` smallint(9) not null,
 	`total` smallint(9) not null,
-	`id_users` smallint(9) default null,
+	`id_users` smallint(9) unsigned default null,
 	`created_at` timestamp NOT NULL DEFAULT now(),
 	`updated_at` timestamp NOT NULL DEFAULT now(),
 	primary key (`id`),
@@ -50,8 +50,8 @@ create table `shopping_cart` (
 
 create table `products_shopping_cart` (
 	`id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
-	`id_product` smallint(9) not null,
-	`id_shopping_cart` smallint(9) not null,
+	`id_product` smallint(9) unsigned not null,
+	`id_shopping_cart` smallint(9) unsigned not null,
 	primary key (`id`),
 	key `products_shopping_cart_FK` (`id_product`),
 	key `products_shopping_cart_FK_1` (`id_shopping_cart`),
@@ -61,8 +61,8 @@ create table `products_shopping_cart` (
 
 create table `products_size` (
 	`id` smallint(6) unsigned NOT NULL auto_increment,
-	`id_product` smallint(6) not null,
-	`id_size` smallint(6) not null,
+	`id_product` smallint(6) unsigned not null,
+	`id_size` smallint(6) unsigned not null,
 	primary key (`id`),
 	key `products_size_FK` (`id_product`),
 	key `products_size_FK_1` (`id_size`),
